acf34e433d373103b2aa6707ba13b2da025a4d14
youngbloodcyb <cameron.youngblood@gmail.com>
2022-08-03 19:28:49 -0600

    add styles to index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,9 +5,9 @@
 <div class="w-full h-screen flex flex-row justify-center align-middle">
     <div class="w-10/12 my-16">
         <div>
-            <p class="font-playfair text-5xl">test</p>
-            <p class="font-mono text-2xl">test</p>
-            <Test test={"test"}/>
+            <h1 class="font-playfair text-8xl my-6">create a playlist<br> or something</h1>
+            <p class="font-mono text-2xl text-orange-600 my-6 font-bold">Create a cool playlist by selecting stuff.</p>
         </div>
+        <a href="/" class="py-3 px-6 transition-all duration-300 ease-in-out font-mono border-black border-solid border-2 rounded-none  hover:shadow-[-6px_6px_#000] block md:inline-block my-4 hover:text-orange-600 hover:text-bold w-full sm:w-40 text-center">Get started</a>
     </div>
 </div>
